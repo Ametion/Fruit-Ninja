9178626279657d44f00c89847a1c90ea4ff58100
Yehor <ametionqq@gmail.com>
2022-05-25 19:09:17 +0200

    Add pause mechanic

---
+++ Assets/Scripts/PauseComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Model;
+using UnityEngine;
+
+public class PauseComponent : MonoBehaviour
+{
+    private GameSession gameSession;
+
+    private void Awake() => gameSession = FindObjectOfType<GameSession>();
+
+    public void Pause() => gameSession.PauseData.IsPause = true;
+    
+    public void Resume() => gameSession.PauseData.IsPause = false;
+}
